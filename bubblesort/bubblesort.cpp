7e83f5026eba650945203405e584f88675e0de5a
hyoungmin <gudals123456@gmail.com>
2015-01-15 17:37:30 +0900

    asdf

--- bubblesort/bubblesort.cpp
+++ bubblesort/bubblesort.cpp
@@ -8,27 +8,24 @@ void main() {
 	printf("hello C world!\n");
 	printf("Bubble sort\n\n");
 
-	int arr[] = {14,27,35,1,23,19,85,67,4,10};
-
+	int arr[] = {34,27,35,1,23,19,85,67,4,10};
+	
+	printarr(arr,sizeof(arr)/4);
+	printf("\n");
 	for(int i = 0; i<sizeof(arr)/4; i++)
 	{
-		for(int j = 0; j<sizeof(arr)/4; j++)
+		for(int j = 0; j<sizeof(arr)/4 - 1; j++)
 		{
-
-			if(arr[i] <= arr[j])
+			if(arr[j] >= arr[j+1])
 			{
-				swap(&arr[i],&arr[j]);
+				swap(&arr[j],&arr[j+1]);
+				printarr(arr,sizeof(arr)/4);
 			}
-
+			
 		}
-
+		printf("\n");
 	}
 
-	for(int i = 0; i<sizeof(arr)/4; i++)
-	{
-		printf("arr[%d] = %d\n",i,arr[i]);
-	}
-	printf("\n\n");
 	printarr(arr,sizeof(arr)/4);
 
 }
@@ -43,10 +40,10 @@ void swap(int * a, int * b)
 
 void printarr(int * arr, int size)
 {
-
+	printf("arr = {");
 	for(int i = 0; i<size; i++)
 	{
-		printf("arr[%d] = %d\n",i,arr[i]);
+		printf(" %d ",arr[i]);
 	}
-
+	printf("}\n");
 }
